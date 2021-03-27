407cfbea98d9d693a11c20d3b42a63755db9c52d
Arthur Panfilov <mrpanfilov@gmail.com>
2021-03-27 15:38:02 +0300

    d2t1, d3t1, d2t2, d3t2

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,21 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        System.out.println("Введите количество этажей в здании:");
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
 
+        if (count >= 1 && count <= 4) {
+            System.out.println("Малоэтажный дом");
+        } else if (count >= 5 && count <= 8) {
+            System.out.println("Среднеэтажный дом");
+        } else if (count > 9) {
+            System.out.println("Многоэтажный дом");
+        } else {
+            System.out.println("Ошибка ввода");
+        }
     }
 }
--- src/main/java/day2/Task2.java
+++ src/main/java/day2/Task2.java
@@ -1,7 +1,23 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        System.out.println("Введие числа a и b:");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
 
+        if (a >= b) {
+            System.out.println("Некорректный ввод");
+        }
+
+        for (int i = a+1; i <= b-1; i++){
+            int count = i;
+            if ((count % 5 == 0) && (count % 10 != 0)) {
+                System.out.print(count + " ");
+            }
+        }
     }
 }
